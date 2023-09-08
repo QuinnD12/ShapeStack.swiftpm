aaf3aac2ba3b3ec39ef67edafdde2d505f10f820
Quinn Davis <qdavis6106@stu.d214.org>
2023-09-08 10:18:47 -0500

    Initial Commit

---
+++ Shape.swift
@@ -0,0 +1,20 @@
+//
+//  Shape.swift
+//  ShapeStack
+//
+//  Created by Quinn B. Davis on 9/8/23.
+//
+
+import SwiftUI
+
+struct Shape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Shape_Previews: PreviewProvider {
+    static var previews: some View {
+        Shape()
+    }
+}
